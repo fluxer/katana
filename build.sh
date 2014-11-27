53462e34a56b28ffb9cb19a211b57f0aa994384b
Ivailo Monev <xakepa10@gmail.com>
2014-11-27 14:13:50 +0000

    script the build of libkdcraw

--- build.sh
+++ build.sh
@@ -2,8 +2,20 @@
 
 set -e
 
-rm -rf kdelibs kde-baseapps kde-workspace kde-extraapps
-rm -rf kdelibs-build baseapps-build workspace-build extraapps-build
+rm -rf libkdcraw kdelibs kde-baseapps kde-workspace kde-extraapps
+rm -rf libkdcraw-build kdelibs-build baseapps-build workspace-build extraapps-build
+
+wget http://download.kde.org/stable/4.14.3/src/libkdcraw-4.14.3.tar.xz
+tar -xf libkdcraw-4.14.3.tar.xz
+mkdir libkdcraw-build && cd libkdcraw-build
+cmake ../libkdcraw-4.14.3 \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DKDE4_BUILD_TESTS=OFF \
+        -DCMAKE_SKIP_RPATH=ON \
+        -DCMAKE_INSTALL_PREFIX=/usr
+make
+make install
+cd ..
 
 git clone --depth=1 https://github.com/fluxer/kdelibs
 mkdir kdelibs-build && cd kdelibs-build
