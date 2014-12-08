fc4a1a759aafaa5f7677396c24daa3e6ab8f3560
Ivailo Monev <xakepa10@gmail.com>
2014-12-08 02:40:48 +0000

    add script to find pots; rename localize to extractpot

---
+++ findpots.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+cwd="$(pwd)"
+if ! type -p find ;then
+    echo "find is not in your PATH"
+    exit 1
+elif ! type -p grep ;then
+    echo "grep is not in your PATH"
+    exit 1
+elif ! type -p sed ;then
+    echo "sed is not in your PATH"
+    exit 1
+fi
+
+packs=("kdelibs" "kde-baseapps" "kde-workspace" "kde-extraapps")
+
+source "$(dirname $0)/fetch.sh"
+
+echo > pots.txt
+for p in ${packs[@]};do
+    echo "Finding POTs in $p..."
+    find "$p" -name Messages.sh -exec grep -o 'podir/.*.pot' {} + | sed 's|podir/||g' >> pots.txt
+done
+
+echo "All done"
