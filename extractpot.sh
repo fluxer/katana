42d07f65bdf8fa84136275f82e9df4d09b7a6dce
Ivailo Monev <xakepa10@gmail.com>
2014-12-09 18:13:34 +0000

    improve extractpot script

--- extractpot.sh
+++ extractpot.sh
@@ -23,9 +23,11 @@ gargs="--from-code=UTF-8 -C -kde \
     -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 \
     -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 \
     -kki18np:1,2 -kki18ncp:1c,2,3 --msgid-bugs-address=xakepa10@gmail.com"
-export EXTRACTRC="perl $cwd/extractrc.pl" XGETTEXT="$(type -p xgettext) $gargs"
-export podir="$cwd"
+export EXTRACTRC="perl $cwd/extractrc.pl" EXTRACTATTR="perl $cwd/extractattr.pl"
+export XGETTEXT="$(type -p xgettext) $gargs" XGETTEXT_QT="$(type -p xgettext) $gargs"
+export podir="$cwd/pots/"
 
+mkdir -pv "$podir"
 cd "$(dirname $message)"
 source "Messages.sh"
 
