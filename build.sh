72bab42e31e789a3751e4e16b246096543227767
Ivailo Monev <xakepa10@gmail.com>
2014-12-25 04:28:14 +0200

    remove icons-build upon build and do not fail if build directories exists

--- build.sh
+++ build.sh
@@ -6,11 +6,12 @@ packs=("ariya-icons" "kdelibs" "kde-baseapps" "kde-workspace" "kde-extraapps" "k
 
 source "$(dirname $0)/fetch.sh"
 
-rm -rf libkdcraw-build kdelibs-build baseapps-build workspace-build extraapps-build l10n-build
+rm -rf libkdcraw-build icons-build kdelibs-build baseapps-build \
+    workspace-build extraapps-build l10n-build
 
 wget http://download.kde.org/stable/4.14.3/src/libkdcraw-4.14.3.tar.xz
 tar -xf libkdcraw-4.14.3.tar.xz
-mkdir libkdcraw-build && cd libkdcraw-build
+mkdir -p libkdcraw-build && cd libkdcraw-build
 cmake ../libkdcraw-4.14.3 \
         -DCMAKE_BUILD_TYPE=Release \
         -DKDE4_BUILD_TESTS=OFF \
@@ -20,7 +21,7 @@ make
 make install
 cd ..
 
-mkdir icons-build && cd icons-build
+mkdir -p icons-build && cd icons-build
 cmake ../ariya-icons \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_INSTALL_PREFIX=/usr
@@ -28,7 +29,7 @@ make
 make install
 cd ..
 
-mkdir kdelibs-build && cd kdelibs-build
+mkdir -p kdelibs-build && cd kdelibs-build
 cmake ../kdelibs \
         -DCMAKE_BUILD_TYPE=Release \
         -DKDE4_BUILD_TESTS=OFF \
@@ -40,7 +41,7 @@ make
 make install
 cd ..
 
-mkdir baseapps-build && cd baseapps-build
+mkdir -p baseapps-build && cd baseapps-build
 cmake ../kde-baseapps \
         -DCMAKE_BUILD_TYPE=Release \
         -DKDE4_BUILD_TESTS=OFF \
@@ -50,7 +51,7 @@ make
 make install
 cd ..
 
-mkdir workspace-build && cd workspace-build
+mkdir -p workspace-build && cd workspace-build
 cmake ../kde-workspace \
         -DCMAKE_BUILD_TYPE=Release \
         -DKDE4_BUILD_TESTS=OFF \
@@ -62,7 +63,7 @@ make
 make install
 cd ..
 
-mkdir extraapps-build && cd extraapps-build
+mkdir -p extraapps-build && cd extraapps-build
 cmake ../kde-extraapps \
         -DCMAKE_BUILD_TYPE=Release \
         -DKDE4_BUILD_TESTS=OFF \
@@ -72,7 +73,7 @@ make
 make install
 cd ..
 
-mkdir l10n-build && cd l10n-build
+mkdir -p l10n-build && cd l10n-build
 cmake ../kde-l10n \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_INSTALL_PREFIX=/usr
