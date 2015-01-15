bd9ed099aab7dc733958d2197cbc7ebbdcc78f84
Ivailo Monev <xakepa10@gmail.com>
2015-01-15 00:38:04 +0000

    changelog.sh: make the date argument option defaulting to latest (pre-)release

--- changelog.sh
+++ changelog.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-date="$1"
+date="${1:-2015-01-04}"
 packs=("ariya-icons" "kdelibs" "kde-baseapps" "kde-workspace" "kde-extraapps" "kde-l10n")
 cwd="$(pwd)"
 if [ -z "$date" ];then
-    echo "Pass a date, e.g. 2015-01-04"
+    echo "Pass a date"
     exit 1
 fi
 
@@ -14,7 +14,7 @@ source "$(dirname $0)/fetch.sh"
 
 changes=""
 for p in "${packs[@]}";do
-    echo "Generating changes for $p..."
+    echo "Generating changes for $p as of $date..."
     cd "$p"
     changes+="\nChanges to $p since $date:"
     pchanges="$(git log --since=$date --format='  * %s')"
