b960b297a6ffcc2702ee3cb725cea1ed8b6903bf
Ivailo Monev <xakepa10@gmail.com>
2015-01-29 00:48:06 +0000

    generic: bump

--- changelog.sh
+++ changelog.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-date="${1:-2015-01-19}"
+date="${1:-2015-01-28}"
 packs=("ariya-icons" "kdelibs" "kde-baseapps" "kde-workspace" "kde-extraapps" "kde-l10n")
 cwd="$(pwd)"
 if [ -z "$date" ];then
