0d5e19de03d9cd0dab648ac0e8d286713583f2c9
Ivailo Monev <xakepa10@gmail.com>
2020-12-29 18:21:28 +0200

    packaging: add strigi NetBSD package files
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

---
+++ packaging/netbsd/strigi/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	strigi
+
+.if !defined(STRIGI_BUILDLINK3_MK)
+STRIGI_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.strigi+=	strigi>=4.19.0.d20201217
+BUILDLINK_PKGSRCDIR.strigi?=	../../katana/strigi
+.endif	# STRIGI_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-strigi
