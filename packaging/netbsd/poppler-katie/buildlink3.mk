d924f58b3b1b42042f44cf14e9db39add7937e12
Ivailo Monev <xakepa10@gmail.com>
2021-02-23 07:47:33 +0200

    packaging: add poppler package files for NetBSD
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

---
+++ packaging/netbsd/poppler-katie/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	poppler-katie
+
+.if !defined(POPPLER_KATIE_BUILDLINK3_MK)
+POPPLER_KATIE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.poppler-katie+=	poppler-katie>=4.20.0.d20201217
+BUILDLINK_PKGSRCDIR.poppler-katie?=	../../katana/poppler-katie
+.endif	# POPPLER_KATIE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-poppler-katie
