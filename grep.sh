144933e88177b228e772f94689b0a2e13ed29bf6
Ivailo Monev <xakepa10@gmail.com>
2021-07-12 14:00:04 +0300

    grep.sh: exclude SVG image files from searches
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- grep.sh
+++ grep.sh
@@ -7,6 +7,6 @@ packs=("kdelibs" "kde-baseapps" "kde-workspace" "kde-extraapps")
 for p in "${packs[@]}";do
     echo "Searching $p..."
     cd "$p"
-    git grep $@ || true
+    git grep $@ ":(exclude)*.svg" || true
     cd ..
 done
